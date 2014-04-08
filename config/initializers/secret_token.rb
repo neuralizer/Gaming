b89f3ee02d08739b868721d5205f02992d18a6ce
Brad Lowton <bradlowton@hotmail.com>
2014-04-08 08:03:28 -0700

    Adding the gaming repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gaming::Application.config.secret_key_base = 'fe2d7aab2f7828d56756887193bac3ef2d46834770f47a68bfbbab69969542c4cbaf20b92f28158584d9e86b7b02ec3ad41af3045a795791593f1a2c7ff614f6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gaming::Application.config.session_store :cookie_store, key: '_Gaming_session'
