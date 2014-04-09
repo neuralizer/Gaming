24decc86a3a2744aef5127055ba863437578bddb
Brad Lowton <bradlowton@hotmail.com>
2014-04-08 19:39:53 -0700

    Working on the sign up page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Gaming::Application.routes.draw do
-  get "users/new"
+  resources :users
   root 'static#home' 
+  match '/signup', to: 'users#new', via: 'get'
   match "/about", to: 'static#about', via: 'get' 
   match "/contact", to: 'static#contact', via: 'get'
   match "/articles", to: 'dynamic#articles', via: 'get'
