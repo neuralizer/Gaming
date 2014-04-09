b5322fcef1bbccc56a273a82f85d7ba0eefa0ff8
Brad Lowton <bradlowton@hotmail.com>
2014-04-09 04:31:59 -0700

    Finito repo

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :signed_in_user, only: [:index, :edit, :update]
-  before_action :correct_user, only: [:edit, :update]
-  
+	before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
+	before_action :correct_user, only: [:edit, :update]
+	before_action :admin_user, only: :destroy
   def index
   	  @users = User.paginate(page: params[:page])  	  
   end
@@ -17,6 +17,12 @@ class UsersController < ApplicationController
 
   def edit  	    	  
   end   
+
+  def destroy
+  	  User.find(params[:id]).destroy
+  	  flash[:success] = "User delteed"
+  	  redirect_to users_url
+  end
   
   def update
   
@@ -54,6 +60,10 @@ def create
     def correct_user
     	    @user = User.find(params[:id])
     	    redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+    	    redirect_to(root_url) unless current_user.admin?
     end
     
 end
