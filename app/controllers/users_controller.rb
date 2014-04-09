1a9fa1f435d1367ec83e2b7c1eb20b4a45e721a7
Brad Lowton <bradlowton@hotmail.com>
2014-04-09 03:45:15 -0700

    updating the repository

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,11 @@
 class UsersController < ApplicationController
 
-  before_action :signed_in_user, only: [:edit, :update]
+  before_action :signed_in_user, only: [:index, :edit, :update]
   before_action :correct_user, only: [:edit, :update]
   
-  
+  def index
+  	  @users = User.all  	  
+  end
 
   def show
   	  @user = User.find(params[:id])  
@@ -45,6 +47,7 @@ def create
     
     # Before filters
     def signed_in_user
+    	    store_location
     	    redirect_to signin_url, notice: "Please sign in." unless signed_in?
     end
     
